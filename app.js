66f8fd32ec2e3d03a0897040bdebd7c39e335838
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-01-02 17:33:12 +0530

    Joi Schema added to handler error of indivisual input

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const Room = require('./model/schema');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./ExpressError/ExpressError');
-const wrapAsync = require('./utils/util')
+const wrapAsync = require('./utils/util');
+const {joiSchema} = require('./joiSchema.js')
 
 const app = express();
 const port = 8080;
@@ -32,23 +33,6 @@ async function main() {
    await mongoose.connect('mongodb://127.0.0.1:27017/RentYRoom')
 };
 
-// const data = new Room({
-//     title: "Ski Chalet in Aspen",
-//     description:"Hit the slopes in style with this luxurious ski chalet in the world-famous Aspen ski resort.",
-//     image:"https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxha2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
-//     price: 4000,
-//     location: "Aspen",
-//     country: "United States",
-//   }).save().then(result=>{
-//     console.log(result)
-//   }).catch(err=>{
-//     console.log(err)
-//   });
-//server
-
-
-
-
 
 app.get('/', wrapAsync( async(req, res,next)=>{
     const rooms = await Room.find();
@@ -61,6 +45,7 @@ app.get('/listings',wrapAsync( async(req,res,next)=>{
 }));
 
 app.get('/listings/new',wrapAsync((req,res,next)=>{
+   
     res.render('new.ejs');
 }));
 
@@ -75,20 +60,14 @@ app.get('/listings/:id',wrapAsync( async(req,res,next)=>{
 
 app.post('/listings',wrapAsync( async(req,res,next)=>{
     const{listing} = req.body;
-    if(!listing){
-        next(new ExpressError(404, 'Please enter valid data'))
-    };
-    const room =  new Room(listing);
-    
-    if(!room.title){
-        next(new ExpressError(404, 'Title is missing!'))
-    }else if(!room.description){
-        next(new ExpressError(404, 'Description is missing!'))
-    }else if(!room.location){
-        next(new ExpressError(404, 'Location is missing!'))
-    }else if(!room.country){
-        next(new ExpressError(404, 'Country is missing!'))
+    const {error, value} =  joiSchema.validate(listing);  
+    console.log(error.details[0].message)
+
+    if(error){
+     return next( new ExpressError(404, error.details[0].message))
     }
+
+    const room =  new Room(listing);
     await room.save()
     res.redirect('/listings');
 }));
@@ -103,11 +82,15 @@ app.get('/listings/:id/edit', wrapAsync(async(req,res,next)=>{
 app.put('/listings/:id',wrapAsync( async(req,res,next)=>{
     const {id} = req.params;
     const{listing} = req.body;
-    if(!listing){
-        next(new ExpressError(400, 'Please enter valid data'))
+
+    //joi error handling 
+    const {error} =  joiSchema.validate(listing);  
+
+    if(error){
+     return next( new ExpressError(404, error.details[0].message))
     }
+
     const room = await Room.findByIdAndUpdate(id,{...listing});
-    
     res.redirect(`/listings/${id}`);
 }));
 
@@ -122,20 +105,6 @@ app.all('*', (req, res, next) =>{
     next(new ExpressError(404, 'Page not found!'))
 });
 
-// const handlingErrByName = (err, req, res) => {
-//     if(err.name === 'CastError'){
-//         const {name, status = 400, message = 'Something went wrong!'} = err;
-//         return res.send('error.ejs', {name, status, message})
-//     }
-
-//     next(err)
-// }
-
-// app.use((err, req, res, next) => {
-//     const {status = 400, message = 'Error Occured!'} = err
-//     handlingErrByName(err, req, res)
-//     next(err);
-// });
 
 //Error Handling Middleware
 app.use((err, req, res, next) => {
--- utils/util.js
+++ utils/util.js
@@ -1,9 +1,11 @@
-//Error Handling Functions
-function wrapAsync(fn){
-    return function (req, res, next){
-        fn(req, res, next).catch((err) => next(err))
-    }
-};
+    //Error Handling Functions
+    function wrapAsync(fn){
+        return function (req, res, next){
+            fn(req, res, next).catch(err=>{
+               return next(err);
+            })
+        }
+    };
 
 
 module.exports = wrapAsync;
